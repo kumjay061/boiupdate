934cdd15149ddc30de86b3469bf23480a73a34fe
kumjay061 <144765557+kumjay061@users.noreply.github.com>
2024-08-06 13:18:33 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
- // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
     apiKey: "AIzaSyC90GnQYUR1ysJYT7bX0r1bDIllxMo83w4",
     authDomain: "getupbaba1.firebaseapp.com",
     projectId: "getupbaba1",
     storageBucket: "getupbaba1.appspot.com",
     messagingSenderId: "465709312280",
-    appId: "1:465709312280:web:8696dbe25d315724e6566d",
-    measurementId: "G-MBKFZ0Q6E2"
+    appId: "1:465709312280:web:8696dbe25d315724e6566d"
+ 
   };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+
+
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
