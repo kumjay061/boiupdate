10b2cfe787b3eadbbcf3962e26050caac088d8ad
kumjay061 <144765557+kumjay061@users.noreply.github.com>
2024-08-06 13:42:06 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC90GnQYUR1ysJYT7bX0r1bDIllxMo83w4",
-    authDomain: "getupbaba1.firebaseapp.com",
-    projectId: "getupbaba1",
-    storageBucket: "getupbaba1.appspot.com",
-    messagingSenderId: "465709312280",
+  apiKey: "AIzaSyC90GnQYUR1ysJYT7bX0r1bDIllxMo83w4",
+  authDomain: "getupbaba1.firebaseapp.com",
+  databaseURL: "https://getupbaba1-default-rtdb.firebaseio.com",
+  projectId: "getupbaba1",
+  storageBucket: "getupbaba1.appspot.com",
+  messagingSenderId: "465709312280",
     appId: "1:465709312280:web:8696dbe25d315724e6566d"
  
   };
